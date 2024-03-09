36627f6fa74012cfe2ea5821dedc9b0fa082deb4
juliusmarminge <julius0216@outlook.com>
2024-03-09 10:20:39 +0100

    repro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,8 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.tsx",
+    "../node_modules/some-pkg/**",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+} satisfies Config;
